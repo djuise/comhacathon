e2dcc4b795c7a1297e52dd726a1ea7d35fbbb5d4
dimagord <dmytrohordiienko@yahoo.com>
2021-09-18 17:34:52 +0300

    Logger

---
+++ src/main/java/com/hackathon/Logger/LoggerTest.java
@@ -0,0 +1,11 @@
+package com.hackathon.Logger;
+
+public class LoggerTest {
+    public static void main(String[] args) {
+        Logger log = new Logger();
+        log.info("This is info");
+        log.warn("This is warn");
+        log.error("This is error");
+        log.
+    }
+}
