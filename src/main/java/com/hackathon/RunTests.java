b5439a05506955090bcf8b98679838697293fd43
Yevhenii Blynnyi <v-yblynnyi@expediagroup.com>
2021-09-19 03:53:52 +0300

    dimon

--- src/main/java/com/hackathon/RunTests.java
+++ src/main/java/com/hackathon/RunTests.java
@@ -1,15 +1,39 @@
 package com.hackathon;
 
-import com.hackathon.runner.Runner;
-import com.hackathon.tests.RunFirstTest;
+import com.hackathon.runner.ParallelRunner;
 
 /**
  * Hello world!
  *
  */
-public class RunTests
-{
-    public static void main( String[] args ) {
-        new Runner().run(new RunFirstTest());
+public class RunTests {
+    public static void main(String[] args) {
+
+//        ParallelRunner.run(args);
+        String[] s = new String[]{"2", "tests.RunBannerTest", "tests.RunNegativeLoginTest"};
+        ParallelRunner.run(s);
+//        Arguments arguments = new Arguments(s);
+
+
+//        Runnable r = null;
+//        try {
+//            BaseTest clazz = (BaseTest) Class.forName("com.hackathon.tests.RunFirstTest").newInstance();
+//            r = new Runner<>(clazz);
+//        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+//            e.printStackTrace();
+//        }
+////        Runnable r = new Runner<>(new RunFirstTest());
+//        Thread t = new Thread(r);
+//        t.start();
+////        Runnable r2 = new Runner<>(new RunFirstTest2());
+////        Thread t2 = new Thread(r2);
+////        t2.start();
+//
+//        try {
+//            t.join();
+////            t2.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
